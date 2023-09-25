0a20a9996be2cc65f2102b7eb413dd50eabe7fae
HayaAgencies <riyashasanath@gmail.com>
2023-09-25 17:24:47 +0530

    hi

--- calculator.js
+++ calculator.js
@@ -1,15 +1,18 @@
 let OperatorIsNotClicked=Boolean();
+let isthisgreaterthanone = Boolean()
 
 let IsPercentageCALCULATOR = false
 let no1 ="";
 let no2 = "";
 let operator ="";
 let numberindex = 1
+var resultno1 = 0;
 let from_btn = ""
 var index = 0;
 var clearone=0;
 
 OperatorIsNotClicked = true;
+isthisgreaterthanone = false
 
 function BtnClick(val){
 
@@ -48,32 +51,50 @@ function FindResult(){
         OperatorIsNotClicked = true
         IsPercentageCALCULATOR = false
     }
+    else if (isthisgreaterthanone){
+        no1 = resultno1
+        var text = resultno1+operator+no2
+        result = eval(text)
+        document.getElementById("Answersbox").value = result
+    }
     else{
         var text = no1+operator+no2
         var result = eval(text)
+        resultno1 = result
+        OperatorIsNotClicked = false
         document.getElementById("Answersbox").value = result
+        isthisgreaterthanone = true
     }
-    
-    no1 ="";
     no2 = "";
     operator ="";
     from_btn = ""
     index = 0;
     clearone=0;
-    OperatorIsNotClicked = true
 }
 function ClearText(){
+    isthisgreaterthanone = false
     document.getElementById("Answersbox").value = ""
     no1 ="";
     no2 = "";
     numberindex = 1
     operator ="";
     from_btn = ""
+    resultno1 = 0;
     index = 0;
     clearone=0;
     OperatorIsNotClicked = true
 }
 function SetNumber(){
+    if(isthisgreaterthanone){
+        if(index == 0){
+            document.getElementById("Answersbox").value = ""
+            no1 = 0 
+            isthisgreaterthanone = false
+            no2 = 0
+            operator = 0
+            OperatorIsNotClicked = true
+        }
+    }
 
     if(OperatorIsNotClicked){
 
